690bca1361f640324fb9a044ae2ea921d320221a
jurgen1c <jurgen1c@gmail.com>
2021-03-20 18:51:22 -0600

    Add linters, active storage & devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :username, :email, :password,
+               :password_confirmation, :avatar, :bio)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :username, :email, :password,
+               :current_password, :avatar, :bio)
+    end
+  end
 end
