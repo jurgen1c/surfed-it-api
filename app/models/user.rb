77ab5e93cf62abbc48f82e19071dce2323470efc
jurgen1c <jurgen1c@gmail.com>
2021-03-20 19:30:56 -0600

    Update test gems & add routes for users and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  name                   :string
+#  username               :string
+#  bio                    :text
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 class User < ApplicationRecord
   include Users::Allowlist
   # Include default devise modules. Others available are:
