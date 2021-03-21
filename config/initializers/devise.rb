96c1af1e0cec16a89068bb411e0813774a2ad978
jurgen1c <jurgen1c@gmail.com>
2021-03-21 07:15:38 -0600

    update devise initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -281,6 +281,9 @@ Devise.setup do |config|
   #   manager.intercept_401 = false
   #   manager.default_strategies(scope: :user).unshift :some_external_strategy
   # end
+  config.warden do |manager|
+    manager.failure_app = DeviseCustomFailure
+  end
 
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
